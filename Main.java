bae976c93688cb6b28233a027766403e91540da8
DESKTOP-GDMTS6V\USER <slowlight50@gmail.com>
2021-12-24 17:44:51 +0900

    main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+import AlgoPractice.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader br = new BufferedReader(new FileReader("input"));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            
+            
+            br.close();
+                        
+            
+            bw.write("");
+
+            bw.flush();            
+            bw.close();
+        }catch(IOException e){
+
+        }
+    }
+}
