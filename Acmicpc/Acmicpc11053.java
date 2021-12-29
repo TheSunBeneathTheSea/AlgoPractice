30d8f3215c38328c38358dd1c57bc0306464c9bc
DESKTOP-GDMTS6V\USER <slowlight50@gmail.com>
2021-12-29 21:17:53 +0900

    백준 11053번

--- Acmicpc/Acmicpc11053.java
+++ Acmicpc/Acmicpc11053.java
@@ -1,4 +1,7 @@
 package Acmicpc;
+
+import java.util.Arrays;
+
 /*
 
 slowlight50@gmail.com
@@ -9,38 +12,46 @@ https://github.com/TheSunBeneathTheSea/AlgoPractice
 
 문제
 
-이 문제는 아주 평범한 배낭에 관한 문제이다.
+수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열을 구하는 프로그램을 작성하시오.
 
-한 달 후면 국가의 부름을 받게 되는 준서는 여행을 가려고 한다. 세상과의 단절을 슬퍼하며 최대한 즐기기 위한 여행이기 때문에, 가지고 다닐 배낭 또한 최대한 가치 있게 싸려고 한다.
-
-준서가 여행에 필요하다고 생각하는 N개의 물건이 있다. 각 물건은 무게 W와 가치 V를 가지는데, 해당 물건을 배낭에 넣어서 가면 준서가 V만큼 즐길 수 있다. 아직 행군을 해본 적이 없는 준서는 최대 K만큼의 무게만을 넣을 수 있는 배낭만 들고 다닐 수 있다. 준서가 최대한 즐거운 여행을 하기 위해 배낭에 넣을 수 있는 물건들의 가치의 최댓값을 알려주자.
+예를 들어, 수열 A = {10, 20, 10, 30, 20, 50} 인 경우에 가장 긴 증가하는 부분 수열은 A = {10, 20, 10, 30, 20, 50} 이고, 길이는 4이다.
 입력
 
-첫 줄에 물품의 수 N(1 ≤ N ≤ 100)과 준서가 버틸 수 있는 무게 K(1 ≤ K ≤ 100,000)가 주어진다. 두 번째 줄부터 N개의 줄에 거쳐 각 물건의 무게 W(1 ≤ W ≤ 100,000)와 해당 물건의 가치 V(0 ≤ V ≤ 1,000)가 주어진다.
+첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000)이 주어진다.
 
-입력으로 주어지는 모든 수는 정수이다.
+둘째 줄에는 수열 A를 이루고 있는 Ai가 주어진다. (1 ≤ Ai ≤ 1,000)
 출력
 
-한 줄에 배낭에 넣을 수 있는 물건들의 가치합의 최댓값을 출력한다.
+첫째 줄에 수열 A의 가장 긴 증가하는 부분 수열의 길이를 출력한다.
 
 
 */
 
 public class Acmicpc11053 {
-    public static int solve(int length, int maxWeight, int[] weight, int[] value){
-        // [0 ~ knapsack에 담을 물건 개수][0 ~ 최대 무게]
-        int[][] dp = new int[length + 1][maxWeight + 1];
+    public static int solve (int[] array) {
+        int[] dp = new int[array.length];
 
-        for (int i = 1; i <= length; i++) {
-            for (int j = 1; j <= maxWeight; j++){
-                if(weight[i - 1] > j) {
-                    dp[i][j] = dp[i - 1][j];
-                }else{
-                    dp[i][j] = Math.max(dp[i - 1][j - weight[i - 1]] + value[i - 1], dp[i - 1][j]);
+        Arrays.fill(dp, 1);
+        
+        for (int i = 0; i < array.length; i++) {
+            int max = 1;
+            for (int j = 0; j < i; j++) {
+                if(array[j] < array[i] && max <= dp[j]){
+                    max = dp[j] + 1;
                 }
+            }
+            dp[i] = max;
+        }
+        
+        int max = 0;
+
+        for (int i : dp) {
+            if(i > max){
+                max = i;
             }
         }
 
-        return dp[dp.length - 1][dp[0].length - 1];
+
+        return max;
     }
 }
